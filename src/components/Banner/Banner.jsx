6391f228a291e797ae6318fed23d5658a3f6e87e
unknown <bushraaktermimno@gmail.com>
2025-10-20 23:54:28 +0600

    added banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -6,6 +6,7 @@ import heroImg from '../../assets/hero.png'
 const Banner = () => {
     return (
         <div className='parentDiv bg-[#E9E9E9] '>
+
        {/* banner */}
         <div className='text-center pt-40 pb-10 max-w-11/12 mx-auto'>
             <h1 className='text-5xl md:text-7xl text-[#001931] font-semibold '>We Build 
@@ -13,34 +14,32 @@ const Banner = () => {
                     bg-clip-text text-transparent'>Productive</span> Apps</h1>
             <p className='text-[#627382] text-base md:text-lg mt-4'>At AppRadar, we craft innovative apps designed to make everyday life simpler, smarter, and more exciting. Our goal is to turn your ideas into digital experiences that truly make an impact.</p>        
         </div>
-        {/* 2 buttons */}
+
         <div className='max-w-11/12 mx-auto flex flex-col md:flex-row items-center justify-center gap-4 pb-10'>
 
-            {/* play store btn */}
             <button onClick={() => window.open("https://play.google.com", "_blank")}
             className='flex gap-2.5 border-2 border-gray-400 rounded-sm py-4 px-4 bg-[#D2D2D2] cursor-pointer hover:scale-105'>
                 <img src={playStoreImg} alt="" />
                 <h4 className='font-semibold text-xl text-[#001931] '>Google Play</h4>
             </button>
 
-            {/* app store btn */}
             <button onClick={() => window.open("https://apps.apple.com", "_blank")}
             className='flex gap-2.5 border-2 border-gray-400 rounded-sm py-4 px-5 bg-[#D2D2D2] cursor-pointer hover:scale-105'>
                 <img src={appStoreImg} alt="" />
                 <h4 className='font-semibold text-xl text-[#001931] '>App Store</h4>
             </button>
         </div>
-        {/* hero img */}
+        
         <div className='max-w-11/12 mx-auto flex flex-col items-center justify-center 
         '>
             <img src={heroImg} alt="" />
         </div>
 
-        {/* states */}
+        
         <div className=' bg-gradient-to-r from-[#632EE3] to-[#9F62F2] py-20'>
             <div className='max-w-11/12 mx-auto text-center'>
                 <h2 className='font-bold text-4xl md:text-5xl text-white'>Trusted by Millions, Built for You</h2>
-                {/* 3 states btn */}
+                
                 <div className='mt-10 flex items-center justify-center gap-12'>
                     <div>
                         <p className='text-gray-300'>Total Downloads</p>
