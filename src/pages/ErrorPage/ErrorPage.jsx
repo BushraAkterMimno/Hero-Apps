d3fa9889b2eeb7d278886b9e81cac608b33dea7d
unknown <bushraaktermimno@gmail.com>
2025-10-21 00:09:21 +0600

    added errorPage

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -3,7 +3,6 @@ import errorImg from '../../assets/error-404.png'
 import Navbar from '../../components/Header/Navbar';
 import Footer from '../../components/Footer/Footer';
 import { Link, useNavigate, useNavigation } from 'react-router-dom';
-// import Spinner from '../../components/Spinner/Spinner';
 
 
 const ErrorPage = () => {
@@ -17,24 +16,22 @@ const ErrorPage = () => {
 
     return (
         <>
-            
-        {/* <Spinner navigationState={navigation.state} /> */}
 
         <Navbar></Navbar>
         <div className='bg-[#E9E9E9]'>
             <div className='max-w-10/12 mx-auto flex flex-col items-center justify-center pt-24 pb-18'>
-            <img src={errorImg} alt="" />
-            <div className='mt-12 text-center'>
-                <h1 className='text-[#001931] font-semibold text-5xl'>Oops, page not found!</h1>
-                <p className='text-[#627382] text-xl mt-3'>The page you are looking for is not available.</p>
+                <img src={errorImg} alt="" />
+                <div className='mt-12 text-center'>
+                    <h1 className='text-[#001931] font-semibold text-5xl'>Oops, page not found!</h1>
+                    <p className='text-[#627382] text-xl mt-3'>The page you are looking for is not available.</p>
                 
-                <button onClick={handleGoBack}
-                className='mt-4 font-semibold text-base text-white rounded-sm py-4 px-10
-                bg-gradient-to-r from-[#632EE3] to-[#9F62F2] cursor-pointer hover:scale-105'>Go Back!
-                </button>
+                    <button onClick={handleGoBack}
+                    className='mt-4 font-semibold text-base text-white rounded-sm py-4 px-10
+                    bg-gradient-to-r from-[#632EE3] to-[#9F62F2] cursor-pointer hover:scale-105'>Go Back!
+                    </button>
                 
+                </div>
             </div>
-        </div>
         </div>
         
         <Footer></Footer>
