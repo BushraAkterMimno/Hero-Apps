b855063502d87b1bf24b5e54b0fae72c51af1493
unknown <bushraaktermimno@gmail.com>
2025-10-21 00:00:55 +0600

    added spinner

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -3,10 +3,12 @@ import { HashLoader } from "react-spinners";
 
 const Spinner = () => {
   return (
+
     <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-white/90 backdrop-blur-sm">
       <HashLoader color="#632EE3" size={100} />
       <p className="mt-4 text-blue-700 font-medium">Loading...</p>
     </div>
+    
   );
 };
 
